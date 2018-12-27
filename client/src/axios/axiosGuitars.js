adfea95db2da86e5a67866bb3058eb9996ed8fc6
Ian R. Claros <ian@sge.net.au>
2018-12-27 21:20:13 +0800

    27/12/2018 9.20pm

--- client/src/axios/axiosGuitars.js
+++ client/src/axios/axiosGuitars.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+//http://localhost:4000
 const Instance = axios.create({
-    baseURL: 'http://localhost:4000'
+    baseURL: 'https://pacific-savannah-54771.herokuapp.com'
 });
 
 export default Instance;
