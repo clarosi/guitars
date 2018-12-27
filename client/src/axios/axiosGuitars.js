514e2ae6a2390e6ac1ed22f75acef58c04d30a43
Ian R. Claros <ian@sge.net.au>
2018-12-28 06:00:06 +0800

    28/12/2018 6.00am

--- client/src/axios/axiosGuitars.js
+++ client/src/axios/axiosGuitars.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { production } from '../shared/utils/stringConstants';
 
 const Instance = axios.create({
-    baseURL: process.env.NODE_ENV === production ? 'https://pacific-savannah-54771.herokuapp.com' : 'http://localhost:4000'
+    baseURL: process.env.NODE_ENV === production ? 'https://ian-claros-demo-ecommerce.herokuapp.com' : 'http://localhost:4000'
 });
 
 export default Instance;
